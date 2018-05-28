225be9903d22bb65e1e3760684f80e7d3bfd40cd
Mingyismith <39313740+Mingyismith@users.noreply.github.com>
2018-05-28 14:42:57 -0400

    Wrapped Up Modal
    - fixed stopTimer, added modal message and restart functionalities.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 let deck = document.querySelector(".deck");
 let card = document.querySelectorAll(".card");
 let cards = [...card];
+let moveCount = document.querySelector('.moves').innerHTML;
 let moves = 0;
 const stars = document.getElementsByClassName('fa fa-star');
 let second = 0;
 let minute= 0;
+let openCards = [];
+let matchCards = [];
 let modal = document.querySelector('.modal');
+let reset = document.querySelector('.restart');
+let timer = document.querySelector('.timer');
+let interval;
+let starCount;
+
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
  */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -28,7 +35,7 @@ function shuffle(array) {
     return array;
 }
 
-
+/*initialize the game with newly shuffled cards */
 function initGame() {
   //shuffle deck
   let shuffledCards = shuffle(cards);
@@ -43,18 +50,22 @@ function initGame() {
 }
 
 initGame();
+startTimer();
 
 
-let openCards = [];
-let matchCards = [];
 
+/* Start playing the matching Game
+* add event listener to each card to display icons when clicking on each card 
+* based on the two cards shown, call in other functions (cardsDoMatch or cardsNotMatch) to check if the cards match
+* add number of matched cards to MatchCards to help with ending the game  
+*/
 
-/* add event listener */
-  cards.forEach(function(card){
-    card.addEventListener('click', function(e){
-        startTimer();
+/* add event listener to each card to display the icons and examine whether the cards match or not by calling other functions*/
+cards.forEach(function(card){
+     card.addEventListener('click', function(e){
+
       /* start timer with the first click */
-        if (!card.classList.contains('show') && !card.classList.contains('open') && !card.classList.contains('match') ){
+      if (!card.classList.contains('show') && !card.classList.contains('open') && !card.classList.contains('match') ){
           openCards.push(card);
           card.classList.add('show','open');
               /* once two cards are open, start evaluating the game */
@@ -63,28 +74,18 @@ let matchCards = [];
               if (openCards[0].innerHTML === openCards[1].innerHTML){
               /* if the two opened cards match */
                   cardsDoMatch();
-
                 }
               /* if there is no match */
               else {
                   cardsNotMatch();
-                }
               }
             }
-
-        })
+          }
       })
+  })
 
 
-function gameSuccess(){
-    clearInterval(interval);
-    finalTime = timer.innerHTML;
-    modal.style.display = "block";
-  
-}
-
-
-
+/* when cards do match, the cards stay open and show their Match style */
 function cardsDoMatch(){
       openCards[0].classList.add('match');
       openCards[0].classList.remove('open','show');
@@ -93,12 +94,13 @@ function cardsDoMatch(){
       matchCards.push(openCards[0]);
       matchCards.push(openCards[1]);
       openCards = [];
-      if (matchCards.length = 16){
-        gameSuccess;
+      /* record game time when all the cards have been matched */
+      if (matchCards.length === 16){
+        endGame();
       }
-    }
+  }
 
-
+/* when cards do not match, cards flip back to original state */
 function cardsNotMatch(){
   setTimeout(function(){
       openCards.forEach(function(card){
@@ -108,28 +110,56 @@ function cardsNotMatch(){
     },500);
   }
 
-
-/*counting moves and change star ratings */
-function starRating(){
-  moves ++;
-  document.querySelector('.moves').innerHTML = moves;
-    if (moves > 6 && moves < 10){
-      stars[2].style.visibility = 'hidden';
+/*  If all 16 cards match, the game ends and a popup window show the congruations message */ 
+function endGame(){
+  clearInterval(interval); /* stop timer */
+  let endTime = timer.innerHTML;
+  let endTimeMsg = document.getElementById('p2');
+  let endMove = document.getElementById('p3');
+  let starFinal = document.getElementById('p4');
+  modal.style.display = "block";
+  endTimeMsg.innerHTML += endTime +".";
+  endMove.innerHTML += moves + " moves.";
+  starFinal.innerHTML += starCount;
+  document.querySelector('.close').onclick = function() {
+    modal.style.display = "none";
     }
-    if (moves >= 10 && moves <15) {
-      stars[1].style.visibility = 'hidden';
-    }
-    if (moves >= 15) {
-      stars[0].style.visibility = "hidden";
+  document.querySelector('.button').onclick = function() {
+    resetGame(); /* reset Game */
     }
 }
 
-var timer = document.querySelector('.timer');
-var interval = null;
+
+
+/* Additional functionalities to keep track of the game progress 
+* Number of moves made with each pairing
+* Number of stars display decreases as game progress
+* Timer
+* Reset button and Restart Icone
+*/
+
+/*counting moves and stars; change star ratings based on number of moves */
+function starRating(){
+  moves ++;
+  moveCount= moves;
+  starCount = 3
+    if (moves > 6 && moves < 10){
+      stars[2].style.visibility = 'hidden';
+      starCount = 2
+    }
+    if (moves >= 10 && moves <15) {
+      stars[1].style.visibility = 'hidden';
+      starCount = 1
+    }
+    if (moves >= 15) {
+      stars[0].style.visibility = "hidden";
+      starCount = 1
+    }
+}
 
 
 function startTimer() {
-    var  interval = window.setInterval(function () {
+  interval = window.setInterval(function () {
           timer.innerHTML = minute + " min " + second + " sec"
             second++;
             if (second == 60) {
@@ -139,6 +169,25 @@ function startTimer() {
         }, 1000);
     }
 
-function stopTimer(){
-    clearInterval(interval);
+/* reset the game either through the modal window or hitting the restar button */
+function resetGame(){
+  modal.style.display = "none";
+  for (let i = 0; i<stars.length ; i++){
+    stars[i].style.visibility = "visible";
+  }
+  second= 0;
+  minute=0;
+  startTimer();
+  moves = 0;
+  openCards = [];
+  matchCards = [];
+  starCount = 3;
+  for (i = 0; i<cards.length; i++){card[i].classList.remove('match','open','show');}
 }
+
+/* reset Game with restart button*/
+reset.onclick = function() {
+  resetGame(); 
+}
+
+  
